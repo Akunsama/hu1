293be2cff3027d73ab247a19c09c7bce33958375
wo4guozhikun <445948042@qq.com>
2018-03-22 00:01:49 +0800

    0322gzk

--- reduxdemo/src/reducers/reducer.jsx
+++ reduxdemo/src/reducers/reducer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { combineReducers } from "redux";
 import { ADD_TODO } from "./actions";
 
@@ -10,32 +9,11 @@ function todos(state = initState, action) {
       return [...todos, action.payload];
     default:
       return state;
-=======
-import { combineReducers } from 'redux'
-import { ADD_TODO } from './actions'
-
-const defaultTodo =[{
-    value:1
-}]
-
-function todos(state = defaultTodo, action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return action.text
-    default:
-      return state
->>>>>>> 24c994fed3d9d4853703c444a3b6a786be656646
   }
 }
 
 const todoApp = combineReducers({
   todos
-<<<<<<< HEAD
 });
 
 export default todoApp;
-=======
-})
-
-export default todoApp
->>>>>>> 24c994fed3d9d4853703c444a3b6a786be656646
