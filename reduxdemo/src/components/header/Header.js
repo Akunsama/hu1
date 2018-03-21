cd5710c8d61838f5f630694f4c326cf1f80ef552
wo4guozhikun <445948042@qq.com>
2018-03-21 22:38:25 +0800

    0321gzk

--- reduxdemo/src/components/header/Header.js
+++ reduxdemo/src/components/header/Header.js
@@ -1,67 +1,84 @@
-import React, {Component} from 'react';
-import Items from '../item/item'
+import React, { Component } from "react";
+import Items from "../item/item";
+import { connect } from "react-redux";
+import { addTodo } from "../../reducers/actions";
 
 class Header extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			value:'',
-			todos:[{
-				value:'1'
-			}]
-		}
-	}
-	
-	changeValue(e){
-		this.setState({
-			value:e.target.value
-		})
-		e.preventDefault();
-	}
-	
-	changeConfirm(e){
-		if(e.keyCode==13){
-			let val = e.target.value;
-			let item = {
-				value:val
-			}
-			this.state.todos.push(item)
-			this.setState({
-				todos:this.state.todos
-			})
-			this.setState({
-				value:''
-			})
-		}
-		e.preventDefault();
-	}
+  // const { dispatch } = this.props
 
-	deleteTodo(itemindex){
-		const arr = this.state.todos.filter((todo,index)=>{
-			return index !== itemindex
-		})
-		this.setState({
-			todos:arr
-		})
-	}
-	
-	render() {
-		return(
-			<div className="com-header">
-			<input type="text" value={this.state.value} onChange={this.changeValue.bind(this)} onKeyUp={this.changeConfirm.bind(this)}/>
-			<ul>
-				
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      todos: [
+        {
+          value: "1"
+        }
+      ]
+    };
+  }
 
-				{
-					this.state.todos.map((todo,index)=>{
-						
-						return (<Items key={index} value={todo.value} index={index} deleteTodo={this.deleteTodo.bind(this)}/>)
-					})
-				}
-			</ul>
+  componentDidMount() {}
+
+  changeValue(e) {
+    this.setState({
+      value: e.target.value
+    });
+    e.preventDefault();
+  }
+  changeConfirm(e) {
+    if (e.keyCode === 13) {
+      let val = e.target.value;
+      let item = {
+        value: val
+      };
+      this.props.addTodo(item);
+    }
+  }
+
+  deleteTodo(itemindex) {
+    const arr = this.state.todos.filter((todo, index) => {
+      return index !== itemindex;
+    });
+    this.setState({
+      todos: arr
+    });
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="com-header">
+                    <input
+          type="text"
+          value={this.state.value}
+          onChange={this.changeValue.bind(this)}
+          onKeyUp={this.changeConfirm.bind(this)}
+        />
+                    <ul>
+                          {this.props.todos.map((todo, index) => {
+            return (
+              <Items
+                key={index}
+                value={todo.value}
+                index={index}
+                deleteTodo={this.deleteTodo.bind(this)}
+              />
+            );
+          })}
+
+        </ul>
       </div>
-		);
-	}
+    );
+  }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+const mapDispatchToProps = {
+  addTodo
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ reduxdemo/src/reducers/reducer.jsx
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+import { ADD_TODO } from "./actions";
+
+const initState = [];
+
+function todos(state = initState, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...todos, action.payload];
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  todos
+});
+
+export default todoApp;
